9fab2e9423c1b2b04ee1a69c4bdc474ef7c49da1
Darshan <dhulswar591@gmail.com>
2020-10-07 18:50:22 +0530

    let and const section done

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-console.log("Working on Babel JS");
+// var and let
 
+for (let i = 0; i < 45; i++) {
+    var div = document.createElement('div');
+    div.onclick = function () {
+        alert("Hello this is " + i + " Box ");
+    };
 
-const double = (num) => num * 2;
+    document.getElementsByTagName('section')[0].appendChild(div);
+}
 
-console.log(double(2));
+// Using the const keyword
+
+const birthYear = 1999;
+const currentYear = 2020;
+
+// birthYear = 1998;
+// Uncaught TypeError: Assignment to constant variable.
+
+const age = currentYear - birthYear;
+console.log(age);
+
+// Working with Template Strings
+function sendEmail(name, price) {
+    const shippingPrice = 50;
+
+    var TotalPrice = price + shippingPrice;
+
+    console.log(`Hi, ${name}, Thank you for buying
+                        Shipping charges: $${shippingPrice}
+                        Grand Total: $${TotalPrice}`);
+}
+
+sendEmail('Darshan', 300);
+
